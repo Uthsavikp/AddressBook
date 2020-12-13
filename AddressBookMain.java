ee16eb1859a9330f19e66360ee12c12bd5312fa4
uthsa <uthsavi29@gmail.com>
2020-12-13 10:51:35 +0530

    Added:To Create A Person Class

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,3 +3,19 @@ public class AddressBookMain {
         System.out.println("Welcome to Address Book Program");
     }
 }
+class Person{
+    // variables
+    public String firstName, lastName, address, city, state;
+    public int zip, phoneNumber;
+
+    //constructor
+    public Person(String firstName, String lastName, String address, String city, String state, int zip, int phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+}
